47d5e4f25a91b9b0da62aff2e5cf828ffc272481
Hugo Martinez <hugo@dejaset.com>
2015-05-28 00:40:20 -0500

    removed unnecessary messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_url
     else
       flash.now[:error] = "Invalid email or password"
       render "new"
@@ -21,6 +21,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url
   end
 end
